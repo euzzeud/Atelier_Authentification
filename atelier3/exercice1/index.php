434e34e7935ee013673d17a392d73cd2ad3047a0
Enzo GIOIELLI <enzogio2004@gmail.com>
2025-11-05 11:50:38 +0100

    fix userType

--- atelier3/exercice1/index.php
+++ atelier3/exercice1/index.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     if ($_SESSION['userType'] == 'admin')
         header('Location: page_admin.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
         exit();
-    } elseif ($_SESSION['userType'] == 'user');
+    } elseif ($_SESSION['userType'] == 'user') {
         header('Location: page_user.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
         exit();
 }
